34630cd9214f73f1e09d40f790ade2a0ed492660
“Taras <tarasdidg@gmail.com>
2023-02-02 21:27:33 +0200

    added code

--- 01_cli_interactive_sort/index.js
+++ 01_cli_interactive_sort/index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const inputText = {
+  greeting: 'Hello. Enter 10 words or didgits deviding them in spaces: ',
+  questionA: 'Sort words alphabetically',
+  questionB: 'Show numbers from lesser to greater',
+  questionC: 'Show numbers from bigger to smaller',
+  questionD: 'Display words in ascending order by number of letters in the word',
+  questionE: 'Show only unique words',
+  questionF: 'Display only unique values from the set of words and numbers entered by the user',
+}
+
+readline.question(inputText.greeting, (input, exit) => {
+  console.log(`Hey there ${input}!`);
+  readline.close();
+});
